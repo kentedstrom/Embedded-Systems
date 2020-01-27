ac583ac3bb9dcd9e48d2fb9f2b8aa4341f6f3504
KENT_EDSTROM <kentedstrom149@gmail.com>
2020-01-27 13:53:26 +0100

    third exercise

--- main.c
+++ main.c
@@ -32,9 +32,23 @@ void integerArray(){
         printf("Integer value: %d, double value: %0.1f \n", array[i], (double)array[i]);
     }
 }
+int checkStrings(char string1[], char string2[]){
+   int i = 0;
+   if (strlen(string1) != strlen(string2)){
+       return 0;
+   }
+   for (int i = 0; i < sizeof(string1) / sizeof(string1[0]); i++){
+       if (!string1[i] == string2[i]){
+           return 0;
+       }
+   }
+   return 1;
+}
 
-
-int main() {
+int main(int argc, char **argv) {
     //copyString();
-    integerArray();
+    //integerArray();
+    if (argc == 3) {
+        printf("/d", checkStrings(argv[1], argv[2]));
+    }
 }
