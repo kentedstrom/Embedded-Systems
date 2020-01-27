4ca56445c28054960e9ce2d9387b5ce129040bf9
KENT_EDSTROM <kentedstrom149@gmail.com>
2020-01-27 12:38:35 +0100

    intial push

---
+++ cmake-build-debug/CMakeFiles/two2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/two2.dir/main.c.o"
+  "libtwo2.dll.a"
+  "two2.exe"
+  "two2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/two2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
