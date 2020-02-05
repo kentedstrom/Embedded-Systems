86d060df77de9aabcd5546f1a9a9f063b22482cf
KENT_EDSTROM <kentedstrom149@gmail.com>
2020-02-05 19:25:11 +0100

    exerc 2.3

--- exerc_2_3.c
+++ exerc_2_3.c
@@ -3,27 +3,39 @@
 #include <stdlib.h>
 
 
-/*
+
 
 int checkStrings(char string1[], char string2[]){
     int i = 0;
     if (strlen(string1) != strlen(string2)){
+        printf("Strings are not the same due to length");
         return 0;
     }
     for (int i = 0; i < sizeof(string1) / sizeof(string1[0]); i++){
-        if (!string1[i] == string2[i]){
+        if (!(string1[i] == string2[i])){
+            printf("Strings are not the same due to character array");
             return 0;
         }
     }
+    printf("Strings are the same according to checkstrings method");
     return 1;
 }
 
-//int main(int argc, char **argv) {
-    //copyString();
-    //integerArray();
-   // if (argc == 3) {
-     //   printf("/d", checkStrings(argv[1], argv[2]));
-    //}
-//}
+int main(int argc, char **argv) {
 
-*/
+    if (argc == 3) {
+        checkStrings(argv[1],argv[2]);
+        if (strcmp(argv[1], argv[2]) == 0){
+          printf("Strings are the same");
+        }
+        else {
+            printf("Strings are not the same");
+        }
+
+
+
+
+    }
+
+
+}
