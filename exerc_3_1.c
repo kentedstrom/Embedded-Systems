74963e9cf3201c817bf7e3c8f966b003d61d625f
joakim deak <joakimdeak@hotmail.com>
2020-02-12 14:23:35 +0100

    fixed 3.1

--- exerc_3_1.c
+++ exerc_3_1.c
@@ -9,7 +9,23 @@ typedef struct {
 } ROBOT;
 
 void move(ROBOT *rob){
-
+    int dir = rob->dir;
+    switch(dir){
+        case 0:
+            rob->ypos--;
+            break;
+        case 1:
+            rob->xpos++;
+            break;
+        case 2:
+            rob->ypos++;
+            break;
+        case 3:
+            rob->xpos--;
+            break;
+        default:
+            break;
+    }
 }
 
 void turn(ROBOT *rob){
@@ -23,44 +39,48 @@ void turn(ROBOT *rob){
 void performInstructions(ROBOT *rob, char inputs[], int inputSize){
     for(int i = 0; i < inputSize; i++){
         if(inputs[i] == 'm'){
-            move(&rob);
+            move(rob);
         } else if(inputs[i] == 't'){
-            turn(&rob);
+            turn(rob);
         }
     }
 }
 
 int main(){
-    ROBOT rob;
-    int x, y, dir;
-    printf("Enter the start position for the robot on the x axis (0-99)\n");
-    scanf("%d", &x);
-    fflush(stdin);
-    printf("Enter the start position for the robot on the y axis (0-99)\n");
-    scanf("%d", &y);
-    fflush(stdin);
-    printf("Enter the start direction for the robot (0-3)\n");
-    scanf("%d", &dir);
-    fflush(stdin);
+    char answer;
+    do{
+        ROBOT rob;
+        int x, y, dir;
+        printf("Enter the start position for the robot on the x axis (0-99)\n");
+        scanf("%d", &x);
+        fflush(stdin);
+        printf("Enter the start position for the robot on the y axis (0-99)\n");
+        scanf("%d", &y);
+        fflush(stdin);
+        printf("Enter the start direction for the robot (0-3)\n");
+        scanf("%d", &dir);
+        fflush(stdin);
 
-    rob.xpos = x;
-    rob.ypos = y;
-    rob.dir = dir;
+        rob.xpos = x;
+        rob.ypos = y;
+        rob.dir = dir;
 
-    printf("The starting position of the robot is (%d, %d) and facing %d\n", rob.xpos, rob.ypos, rob.dir);
-    printf("Enter a set of moves (t and m)\n");
+        printf("The starting position of the robot is (%d, %d) and facing %d\n", rob.xpos, rob.ypos, rob.dir);
+        printf("Enter a set of moves (t and m)\n");
 
-    char inputs[2];
+        char inputs[100];
 
-    fgets(inputs, 2, stdin);
-    while ((getchar()) != '\n');
-    for(int i = 0; i < 2; i++){
-        printf("here %c\n", inputs[i]);
-    }
+        while ((getchar()) != '\n');
+        fgets(inputs, 100, stdin);
 
-    performInstructions(&rob, inputs, sizeof(inputs) / sizeof(inputs[0]));
+        performInstructions(&rob, inputs, sizeof(inputs) / sizeof(inputs[0]));
 
-    printf("Robot position is now X:%d Y:%d\n", rob.xpos, rob.ypos);
+        printf("Robot position is now X:%d Y:%d and facing %d\n", rob.xpos, rob.ypos, rob.dir);
+
+        printf("Do you want to do it again? (Y/N)\n");
+        scanf("%c", &answer);
+        fflush(stdin);
+    } while(answer != 'N');
 
     return 0;
 }
